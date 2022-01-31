78e97f35031bb74ce6bacaa310771a95ea760c91
Raul Armas <you@example.com>
2022-01-31 10:42:02 -0500

    updated with rate and payment functions

---
+++ app.form.financial/app.form.financial/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FUNCIONESToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RATEToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PAYMENTToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FUNCIONESToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1441, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FUNCIONESToolStripMenuItem
+        '
+        Me.FUNCIONESToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RATEToolStripMenuItem, Me.PAYMENTToolStripMenuItem})
+        Me.FUNCIONESToolStripMenuItem.Name = "FUNCIONESToolStripMenuItem"
+        Me.FUNCIONESToolStripMenuItem.Size = New System.Drawing.Size(83, 20)
+        Me.FUNCIONESToolStripMenuItem.Text = "FUNCIONES"
+        '
+        'RATEToolStripMenuItem
+        '
+        Me.RATEToolStripMenuItem.Name = "RATEToolStripMenuItem"
+        Me.RATEToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RATEToolStripMenuItem.Text = "RATE"
+        '
+        'PAYMENTToolStripMenuItem
+        '
+        Me.PAYMENTToolStripMenuItem.Name = "PAYMENTToolStripMenuItem"
+        Me.PAYMENTToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PAYMENTToolStripMenuItem.Text = "PAYMENT"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1441, 545)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FUNCIONESToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RATEToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PAYMENTToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ app.form.financial/app.form.financial/Form1.vb
@@ -0,0 +1,52 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub MYRATE()
+        Dim Fmt, FVal, Guess, PVal, Payment, TotPmts, PayType, APR
+        Const ENDPERIOD = 0, BEGINPERIOD = 1    ' When payments are made.
+        Fmt = "##0.00"    ' Define percentage format.
+        FVal = 0    ' Usually 0 for a loan.
+        Guess = 0.1    ' Guess of 10 percent.
+        PVal = InputBox("How much did you borrow?")
+        Payment = InputBox("What's your monthly payment?")
+        TotPmts = InputBox("How many monthly payments do you have to make?")
+        PayType = MsgBox("Do you make payments at the end of the month?",
+        vbYesNo)
+        If PayType = vbNo Then PayType = BEGINPERIOD Else PayType = ENDPERIOD
+        MsgBox(Rate(TotPmts, -Payment, PVal, FVal, PayType, Guess))
+        APR = (Rate(TotPmts, -Payment, PVal, FVal, PayType, Guess) * 12) * 100
+        MsgBox("Your interest rate is " & Format(APR, Fmt) & " percent (monthly).")
+    End Sub
+
+    Private Sub PAYMENT()
+        Dim Fmt, FVal, PVal, APR, TotPmts, PayType, Payment
+        Const ENDPERIOD = 0, BEGINPERIOD = 1    ' When payments are made.
+        Fmt = "###,###,##0.00"    ' Define money format.
+        FVal = 0    ' Usually 0 for a loan.
+        PVal = InputBox("How much do you want to borrow?")
+        APR = InputBox("What is the annual percentage rate of your loan?")
+        If APR > 1 Then APR = APR / 100    ' Ensure proper form.
+        APR = InputBox("What is the monthly percentage rate of your loan?")
+        If APR > 1 Then APR = APR / 100    ' Ensure proper form.
+        TotPmts = InputBox("How many monthly payments will you make?")
+        PayType = MsgBox("Do you make payments at the end of month?", vbYesNo)
+        If PayType = vbNo Then PayType = BEGINPERIOD Else PayType = ENDPERIOD
+        Payment = Pmt(APR, TotPmts, -PVal, FVal, PayType)
+        MsgBox("Your payment will be " & Format(Payment, Fmt) & " per month.")
+    End Sub
+
+    Private Sub ToolStripContainer1_ContentPanel_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub RATEToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RATEToolStripMenuItem.Click
+        MYRATE()
+    End Sub
+
+    Private Sub PAYMENTToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PAYMENTToolStripMenuItem.Click
+        PAYMENT()
+    End Sub
+End Class
